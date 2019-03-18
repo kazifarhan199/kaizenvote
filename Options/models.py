6a937f691f2f7f9b83ea15edae5e156bbe291e7b
KaziFarhanuddin <kazifarhan199@gmail.com>
2019-03-19 02:20:50 +0530

    Changed d1 d2 d3 as optional fields

--- Options/models.py
+++ Options/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class Options_model(models.Model):
 	title = models.ForeignKey(Title_model, on_delete=models.CASCADE)
 	name = models.CharField(max_length=400)
-	d1 = models.CharField(max_length=400)
-	d2 = models.CharField(max_length=400)
-	d3 = models.CharField(max_length=400)
+	d1 = models.CharField(max_length=400, blank=True)
+	d2 = models.CharField(max_length=400, blank=True)
+	d3 = models.CharField(max_length=400, blank=True)
 	votes = models.IntegerField(default=0)
 	image = models.ImageField()
 
