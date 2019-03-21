3bd829eda4435224d17b8c0e7f85f0d65b5481c3
KaziFarhanuddin <kazifarhan199@gmail.com>
2019-03-21 17:51:08 +0530

    Set Debug to False

--- VootingWebsite/settings.py
+++ VootingWebsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k_(vx7o4b5n68l%a#=bongh)bs+64$$4vlyn@6xj@mz7=#m&=@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.0.106','127.0.0.1', '*']
 
