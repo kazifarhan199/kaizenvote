a11c6e01d8701992e74c6d0a548eb0efcffd47ef
KaziFarhanuddin <kazifarhan199@gmail.com>
2020-02-22 23:01:32 +0530

    Changed DEBUGGING to False also set email backend

--- VootingWebsite/settings/production.py
+++ VootingWebsite/settings/production.py
@@ -6,16 +6,18 @@ import dj_database_url
 SECRET_KEY = 'k_(vx7o4b5n68l%a#=bongh)bs+64$$4vlyn@6xj@mz7=#m&=@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', ]
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
 if 'DATABASE_URL' in os.environ:
     DATABASES = {'default': dj_database_url.config()}
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
