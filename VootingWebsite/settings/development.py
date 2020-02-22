9779143a174354cbe2d8156838c8b58df099164f
KaziFarhanuddin <kazifarhan199@gmail.com>
2020-02-23 00:24:03 +0530

    Added password reset

--- VootingWebsite/settings/development.py
+++ VootingWebsite/settings/development.py
@@ -18,4 +18,12 @@ DATABASES = {
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
+
+MY_SITE_NAME = "KaizenVote"
