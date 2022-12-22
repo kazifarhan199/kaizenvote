39c67065fa035f968cf298e17272676febc58add
Farhanuddin Kazi <kazifarhan199@gmail.com>
2022-12-22 16:35:12 -0600

    test urgentworker migrating

--- VootingWebsite/settings/production.py
+++ VootingWebsite/settings/production.py
@@ -28,13 +28,6 @@ elif 'DATABASE_URL' in os.environ:
     import dj_database_url
     DATABASES = {'default': dj_database_url.config()}
 
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
 
 print(os.getenv('EMAIL'))
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
